f2deb21e9949b36ddb912e7b636f5be01c18587f
lnicosia <lnicosia@student.42.fr>
2018-11-19 18:47:36 +0100

    flood_fill corrected??

--- Level4/flood_fill/flood_fill.c
+++ Level4/flood_fill/flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 11:06:06 by lnicosia          #+#    #+#             */
-/*   Updated: 2018/11/19 14:15:53 by lnicosia         ###   ########.fr       */
+/*   Updated: 2018/11/19 18:45:40 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	fill(char **tab, int i, int j, char c, int xmax, int ymax)
 		fill(tab, i + 1, j, c, xmax, ymax);
 	if (i - 1 >= 0 && tab[i - 1][j] == c)
 		fill(tab, i - 1, j, c, xmax, ymax);
-	if (j + 1 < xmax && tab[i][j + 1] == c)
+	if (j + 1 <= xmax && tab[i][j + 1] == c)
 		fill(tab, i, j + 1, c, xmax, ymax);
 	if (j - 1 >= 0 && tab[i][j - 1] == c)
 		fill(tab, i, j - 1, c, xmax, ymax);
