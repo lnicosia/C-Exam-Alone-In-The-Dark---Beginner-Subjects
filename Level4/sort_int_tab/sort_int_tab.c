2fb0fb6add1bfda383eb0508f0adb33703b3cd30
lnicosia <lnicosia@student.42.fr>
2018-11-19 17:47:11 +0100

    sort_int_tab.

---
+++ Level4/sort_int_tab/sort_int_tab.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_integer_table.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/06 20:17:42 by lnicosia          #+#    #+#             */
+/*   Updated: 2018/11/19 17:46:21 by lnicosia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int i;
+	unsigned int j;
+	int tmp;
+
+	i = 0;
+	j = 0;
+	tmp = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size - i - 1)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				tmp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
